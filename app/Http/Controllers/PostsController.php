ec464d9e6c8c30e12f4f381c2cef1fd1e18ff57d
Romi Alief Rahman <raliefrenha2@gmail.com>
2022-12-25 07:03:34 +0700

    02 optimasi memory usage

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $years = Post::query()
+            ->select('*')
+            ->with('author')
+            ->select('id', 'title', 'slug', 'published_at', 'author_id')
+            ->with('author:id,name')
+            ->latest('published_at')
+            ->get()
+            ->groupBy(fn ($post) => $post->published_at->year);
+
+        return view('posts', ['years' => $years]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Company;
+use App\Post;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         factory(Company::class, 1000)->create()->each(fn ($company) => $company->users()
             ->createMany(factory(User::class, 50)->make()->map->getAttributes())
+        );
+
+        factory(User::class, 20)->create(['company_id' => 1])->each(fn ($user) => $user->posts()
+            ->createMany(factory(Post::class, 5)->make()->toArray())
         );
     }
 }
